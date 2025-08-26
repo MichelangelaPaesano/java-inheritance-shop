ed03354ebbc521facc1e1c9151dac28ddb739402
MichelangelaPaesano <paesanomichelangela@gmail.com>
2025-08-26 03:30:44 +0200

    correzione cuffie

--- java-inheritance-shop/src/sottoProdotti/Cuffie.java
+++ java-inheritance-shop/src/sottoProdotti/Cuffie.java
@@ -4,17 +4,19 @@ import prodotto.Prodotto;
 
 public class Cuffie extends Prodotto{
     private String colore;
-    private String wireless;
+    private boolean wireless;
 
-public Cuffie() {
-    super ("JBL", 170);
+public Cuffie(String nome, double prezzo, String colore, boolean wireless) {
+    super (nome, prezzo);
+    this.colore = colore;
+    this.wireless = wireless; 
 }
 
 public String getColore () {
     return colore;
 }
 
-public String getWireless () {
+public boolean isWireless () {
     return wireless;
 }
 }
