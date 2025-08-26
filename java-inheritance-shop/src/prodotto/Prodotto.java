fe4895fc325770f03b29c300141313e6cfec6b10
MichelangelaPaesano <paesanomichelangela@gmail.com>
2025-08-26 02:15:30 +0200

    getter e setter

--- java-inheritance-shop/src/prodotto/Prodotto.java
+++ java-inheritance-shop/src/prodotto/Prodotto.java
@@ -7,8 +7,8 @@ public class Prodotto {
     private int codice;
     private String nome; 
     private String marca;
-    private int prezzo;
-    private int iva; 
+    private double prezzo;
+    private double iva; 
 
     //creo il costruttore
     public Prodotto(String nome, int prezzo) {
@@ -23,7 +23,63 @@ public class Prodotto {
         System.out.println(codice);
     }
 
+    //rendo accessibile la lettura del "codice"
     public int getCodice() {
         return codice;
     }
+
+    //rendo accessibile la lettura del "nome"
+    public String getNome () {
+        return nome;
+    }
+
+    //rendo accessibile la scrittura del "nome" -- ma vogliamo impostare un po' di controllo
+    //per evitare che chiunque possa dall'esterno modificare i valori usando il setter
+    public void setNome(String nome) {
+        if (nome == null || nome.isEmpty()) {
+            System.out.print("Il nome inserito non valido");
+        } else {
+            this.nome = nome;
+        }
+    }
+
+    //passiamo alla marca
+    public String getMarca () {
+        return marca;
+    }
+
+    public void setMarca (String marca) {
+        if (marca == null || marca.isEmpty()) {
+            System.out.print("La marca inserita non è valida");
+        } else {
+            this.marca = marca; 
+        }
+    }
+
+    //passiamo al prezzo
+    public double getPrezzo () {
+        return prezzo;
+    }
+
+    public void setPrezzo (double prezzo) {
+        if (prezzo < 0) {
+            System.out.println("Il prezzo inserito non è valido");
+        } else {
+            this.prezzo = prezzo;
+        }
+    }
+
+    //passiamo all'iva
+    public double getIva () {
+        return iva;
+    }
+
+    public void setIva (double iva) {
+        if (iva <= 100) {
+            System.out.println("L'iva inserita non è valida");
+        } else {
+            this.iva = iva;
+        }
+    }
 }
+
