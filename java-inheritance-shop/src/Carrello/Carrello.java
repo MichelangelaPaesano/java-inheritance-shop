4147c0f289d633b10ecae80d8df4f16ac616b3ad
MichelangelaPaesano <paesanomichelangela@gmail.com>
2025-08-27 17:21:36 +0200

    carrello refinement

--- java-inheritance-shop/src/Carrello/Carrello.java
+++ java-inheritance-shop/src/Carrello/Carrello.java
@@ -1,6 +1,11 @@
 package Carrello;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+
+import sottoProdotti.Cuffie;
+import sottoProdotti.Smartphone;
+import sottoProdotti.Televisori;
 
 public class Carrello {
     public static void main(String[] args) {
@@ -40,6 +45,13 @@ public class Carrello {
             int prezzo = scan.nextInt();
             
         }
+        
+        ArrayList<Prodotto> prodotti = new ArrayList<>();
+        Smartphone cell1 = new Smartphone("S21", 1200, "123456789123456", 16);
+        prodotti.add(cell1);
+        Cuffie c1 = new Cuffie("Logitech", 55, "nero", true);
+        Televisori tv1 = new Televisori("Xiamo Tv", 1250, 60, true);
+
     } 
 
     }
