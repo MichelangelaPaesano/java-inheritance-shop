6b55c519445cbcdbf7f17deed42a47a157872a9c
MichelangelaPaesano <paesanomichelangela@gmail.com>
2025-08-29 12:20:10 +0200

    commenti per capire le cose fatte

--- java-inheritance-shop/src/Carrello/Carrello.java
+++ java-inheritance-shop/src/Carrello/Carrello.java
@@ -10,17 +10,20 @@ import sottoProdotti.Televisori;
 public class Carrello {
     public static void main(String[] args) {
 
-        //interagiamo con utente per chiedere cosa vuole inserire nel carrello
+        //Creo variabili che poi uso nell'if 
         String nome;
         String marca;
         Double prezzo;
 
+        ////interagiamo con utente per chiedere cosa vuole inserire nel carrello
         Scanner scan = new Scanner(System.in);
         System.out.println("Quale prodotto vuoi aggiungere al carrello?");
         String domanda = scan.nextLine();
 
+        //creo attalist dinamico, così che a mano a mano posso inserire i prodotti
         ArrayList<Prodotto> prodotti = new ArrayList<>();
 
+        //creo le condizioni per la quale l'utente sceglie e in base alla scelta decide anche le caratteristiche 
         if (domanda.equals("Smartphone")) {
             System.out.println("Quale modello vuoi acquistare?");
             nome = scan.nextLine();
@@ -28,7 +31,9 @@ public class Carrello {
             marca = scan.nextLine();
             System.out.println("Quanto vuoi spendere?");
             prezzo = scan.nextDouble();
+            //scan.nextLine dopo il double è per un bug 
             scan.nextLine();
+            //creo oggetto direttamente nell'if
             Smartphone cell1 = new Smartphone(nome, marca, prezzo);
             prodotti.add(cell1);
         } else if (domanda.equals("Televisore")) {
@@ -53,6 +58,7 @@ public class Carrello {
             prodotti.add(c1);
         }
 
+        //ciclo for each per gli elementi che vengono inseriti e con print del ToString 
         for (Prodotto elemento : prodotti) {
             System.out.println(elemento.toString());
         }
